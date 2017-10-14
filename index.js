69880393a798091f07d1051c9020b8a0d7b6b912
Cindy <cancrum82@gmail.com>
2017-10-14 16:02:00 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ var name = "Susan"
 var height = "70"
 
 if (name == "Susan") {
-  console.log("The name is" +name)
-  console.log("The height is" +height)
+  console.log("The name is" + name)
+  console.log("The height is" + height)
 } else {
   console.log("Expected the name to be Susan")
 }
